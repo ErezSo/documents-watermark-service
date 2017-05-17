6e85e671ead710827dc1796d43e8c99b6f6453fb
Erez Sobel <sobelerez@gmail.com>
2017-05-17 11:11:36 +0200

    Setup unit testing

---
+++ Routes/document_routes.test.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { expect } = require('chai');
+const documentRouter = require('document_routes');
+
+describe('documentRouter', () => {
+  describe('First test', () => {
+    it('should pass', (done) => {
+      expect(true).to.equal(true);
+      done();
+    });
+  });
+})
