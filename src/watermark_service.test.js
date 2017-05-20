d588f36c1f169563816341a49fea68ce3f3396ce
Erez Sobel <sobelerez@gmail.com>
2017-05-20 18:19:56 +0200

    Create unit testing suite and test for watermarker service

---
+++ src/watermark_service.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const {expect} = require('chai');
+const watermarker = require('./watermark_service');
+
+describe('watermarker', () => {
+  it('Create a new watermark object', () => {
+    const inputData = {
+      params: {
+        documentType: 'book',
+        topic: 'Science'
+      },
+      bodyVars: {
+        title: 'Science book',
+        author: 'Prof. book author'
+      }
+    }
+
+    const expectedData = {
+      content: 'book',
+      title: 'Science book',
+      author: 'Prof. book author',
+      topic: 'Science'
+    }
+
+    return Promise.resolve(watermarker(inputData.params, inputData.bodyVars)).then(testDataPromise => {
+      expect(testDataPromise).to.eql(expectedData);
+    });
+  });
+})
