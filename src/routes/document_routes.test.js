72751b08d545ebb7b3391e202c9acf267d108d07
Erez Sobel <sobelerez@gmail.com>
2017-05-21 15:50:31 +0200

    Add test for successful GET request

--- src/routes/document_routes.test.js
+++ src/routes/document_routes.test.js
@@ -1,13 +1,38 @@
 'use strict'
 
 const { expect } = require('chai');
-const documentRouter = require('./document_routes');
+const nock = require('nock');
+const request = require('supertest');
 
-describe('documentRouter', () => {
-  describe('First test', () => {
-    it('should pass', (done) => {
-      expect(true).to.equal(true);
-      done();
+describe('documentRouter via app.js', () => {
+  describe('POST request', () => {
+    it('should return 200 and ticket id', (done) => {
+      nock('http://localhost')
+        .post('/watermark/book/science')
+        .reply(200, {
+          ticket: '123456789qwerty'
+        });
+
+      const inputData = {
+        params: '/book/science',
+        body: {
+          title: 'Bad Science',
+          author: 'Ben Goldcare'
+        }
+      };
+
+      const expectedData = {
+        ticket: '123456789qwerty'
+      }
+
+      request('http://localhost')
+        .post(`/watermark${inputData.params}`, inputData.body)
+        .expect(200)     
+        .end((err, res) => {
+          expect(res.body.ticket).to.equal(expectedData.ticket);
+          if (err) return done(err);
+          done();
+       });
     });
   });
 })
