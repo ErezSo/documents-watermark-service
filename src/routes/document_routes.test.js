d3e42857b75e8b25906620d1071ba547d535d610
Erez Sobel <sobelerez@gmail.com>
2017-05-21 15:52:01 +0200

    Add test for successful POST request

--- src/routes/document_routes.test.js
+++ src/routes/document_routes.test.js
@@ -35,4 +35,49 @@ describe('documentRouter via app.js', () => {
        });
     });
   });
+
+ describe('GET request', () => {
+    it('should return 200 and document record including watermark object', (done) => {
+      nock('http://localhost')
+        .get('/status/123456789qwerty')
+        .reply(200, {
+          "_id": "123456789qwerty",
+          "title": 'Bad Science',
+          "author": 'Ben Goldcare',
+          "__v": 0,
+          "watermark": {
+            "content": "book",
+            "title": 'Bad Science',
+            "author": 'Ben Goldcare',
+            "topic": "science"
+          }
+        });
+
+      const inputData = {
+        ticketId: '123456789qwerty'
+      };
+
+      const expectedData = {
+          "_id": "123456789qwerty",
+          "title": 'Bad Science',
+          "author": 'Ben Goldcare',
+          "__v": 0,
+          "watermark": {
+            "content": "book",
+            "title": 'Bad Science',
+            "author": 'Ben Goldcare',
+            "topic": "science"
+          }
+        }
+
+      request('http://localhost')
+        .get(`/status/${inputData.ticketId}`)
+        .expect(200)     
+        .end((err, res) => {
+          expect(res.body).to.eql(expectedData);
+          if (err) return done(err);
+          done();
+       });
+    });
+  });
 })
